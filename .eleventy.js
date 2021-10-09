f4c980fb830dc4002a4a04eef7dd9ef129d5a167
victor619 <victor619@github.com>
2021-10-10 00:25:38 +0100

    A new eleventy site

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,12 @@
 module.exports = function(eleventyConfig){
-
-    eleventyConfig.addPassthroughCopy('../src/style/index.css')
-    eleventyConfig.addWatchTarget('../src/style/index.css')
-
+    eleventyConfig.addPassthroughCopy("./src/style/index.css")
+    eleventyConfig.addWatchTarget("./src/style/index.css")
+    eleventyConfig.addPassthroughCopy("./src/img")
+    
     return{
-       
         dir:{
-            input:"src",
-            output:"public"
+            input: "src",
+            output: "public"
         }
     }
 }
