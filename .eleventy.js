a43fd4e8c640d53ea9a6177e19d3cd721d573881
victor619 <victor619@github.com>
2021-10-12 21:51:09 +0100

    Almost done

--- .eleventy.js
+++ .eleventy.js
@@ -4,9 +4,12 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy("./src/img")
     
     return{
+
         dir:{
+
             input: "src",
             output: "public"
         }
+        
     }
 }
